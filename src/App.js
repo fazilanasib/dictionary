d5c74718642d667db47d4e98fb0461e5e6b876f9
fazilanasib <121870855+fazilanasib@users.noreply.github.com>
2023-05-03 05:57:36 -0400

    set a search engine for dictionary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import React from "react";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">Hello</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <smal>Coded by Fazila Nasib</smal>
+        </footer>
+      </div>
     </div>
   );
 }
