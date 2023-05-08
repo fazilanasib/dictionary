913039354a8bfbc3943496357e052528ad36e643
fazilanasib <121870855+fazilanasib@users.noreply.github.com>
2023-05-08 06:12:17 -0400

    added phonetics

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Hello</header>
+        <header className="App-header"></header>
         <main>
           <Dictionary />
         </main>
