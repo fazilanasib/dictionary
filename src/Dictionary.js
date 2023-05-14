a7e9e5e6039c45f77ed2c41f6b1bbcad9bf437d5
fazilanasib <121870855+fazilanasib@users.noreply.github.com>
2023-05-14 14:45:29 -0400

    api for photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,17 +2,23 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("dance");
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function load() {
@@ -32,6 +38,7 @@ export default function Dictionary() {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
@@ -41,6 +48,14 @@ export default function Dictionary() {
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey = `8AyRZKpCqrvMuX67e1sjwNNfckZlv4XrJ6A4BYJifv14NRtglz62EsGc`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
